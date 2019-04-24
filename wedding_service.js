141aee1ca3627bfb50928fb9aad71dbfae8fbbd5
finns96 <47065360+finns96@users.noreply.github.com>
2019-04-23 20:16:24 -0700

    Add files via upload

--- wedding_service.js
+++ wedding_service.js
@@ -25,6 +25,7 @@ console.log("web service started FATCOCK");
 const bodyParser= require('body-parser');
 const jsonParser= bodyParser.json();
 app.post('wedding.html', jsonParser, function (req, res) {
+	console.log("who knows")
 	let content = req.body["firstname"]+":::"+req.body["lastname"]+":::"+req.body["phone"]+":::"+req.body["email"]+":::"+req.body["plusfirst"]+":::"+req.body["pluslast"]+":::"+req.body["message"]+"\n";
 	fs.appendFile("rsvp.txt", content, function(err){
 		if (err){
