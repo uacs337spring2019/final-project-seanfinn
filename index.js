111059099ce7501cc807ff1185cb0c8cf3942110
finns96 <47065360+finns96@users.noreply.github.com>
2019-04-23 21:19:16 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+Sean Finn
+
+CSC337
+
+chatit_service.js
+
+This file is responsible for all the code to run a node server with this code.
+*/
+
+"use strict";
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.static('public'));
+
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+	next()
+})
+app.use(express.static('public'));
+console.log("web service started FATCOCK");
+
+app.get('/', function(req,res){
+	res.render('index');
+});
+
+const bodyParser= require('body-parser');
+const jsonParser= bodyParser.json();
+console.log("who knows")
+app.post('https://weddingappseanfinn.herokuapp.com/', jsonParser, function (req, res) {
+	let content = req.body["firstname"]+":::"+req.body["lastname"]+":::"+req.body["phone"]+":::"+req.body["email"]+":::"+req.body["plusfirst"]+":::"+req.body["pluslast"]+":::"+req.body["message"]+"\n";
+	fs.appendFile("rsvp.txt", content, function(err){
+		if (err){
+			return console.log(err);
+		}
+		console.log("The file was saved")
+	})
+});
+
+app.listen(process.env.PORT);
