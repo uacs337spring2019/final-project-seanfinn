8db12e86db0364bbc32f58dd5bea429eb7f044b2
finns96 <47065360+finns96@users.noreply.github.com>
2019-04-23 17:01:51 -0700

    Add files via upload

--- wedding.js
+++ wedding.js
@@ -1,6 +1,6 @@
-"use strict";
-//https://heroku.com/deploy?template=https://github.com/uacs337spring2019/final-project-seanfinn
+//SEANFINN
 (function(){
+  "use strict";
   window.onload = function(){
     let welcome = document.getElementById("welcome");
     let ourstory = document.getElementById("ourstory");
@@ -12,7 +12,7 @@
 
 
 
-    var top = welcome.offsetTop;
+    let top = welcome.offsetTop;
     window.scrollTo(10,top-50);
 
 
@@ -42,15 +42,18 @@
     rsvpbutton.onclick = showHide;
     usefullinksbutton.onclick = showHide;
 
-    let submitButton = document.getElementById('submit')
+    let submitButton = document.getElementById('submit');
     submitButton.onclick = submitClick;
     initCountDownTimer();
     initGallery();
     initMap();
 
 
-  }
+  };
 
+  /**
+  jsdoc comment
+  */
   function showHide(){
     let welcome = document.getElementById("welcome");
     let ourstory = document.getElementById("ourstory");
@@ -71,38 +74,38 @@
 
 
     if (this['id'] == "HomeButton"){
-      welcome.style.display = 'flex'
-      var top = welcome.offsetTop;
+      welcome.style.display = 'flex';
+      let top = welcome.offsetTop;
       window.scrollTo(10,top-50);
     }
     else if (this['id'] == "OurStoryButton"){
-      ourstory.style.display = 'flex'
-      var top = ourstory.offsetTop;
+      ourstory.style.display = 'flex';
+      let top = ourstory.offsetTop;
       window.scrollTo(10,top-50);
     }
     else if (this['id'] == "ScheduleButton"){
-      schedule.style.display = 'flex'
-      var top = schedule.offsetTop;
+      schedule.style.display = 'flex';
+      let top = schedule.offsetTop;
       window.scrollTo(10,top-50);
     }
     else if (this['id'] == "RockyPointMapButton"){
-      rpmap.style.display = 'flex'
-      var top = rpmap.offsetTop;
+      rpmap.style.display = 'flex';
+      let top = rpmap.offsetTop;
       window.scrollTo(0,top -50);
     }
     else if (this['id'] == "GalleryButton"){
-      gallery.style.display = 'flex'
-      var top = gallery.offsetTop;
+      gallery.style.display = 'flex';
+      let top = gallery.offsetTop;
       window.scrollTo(10,top-50);
     }
     else if (this['id'] == "RSVPButton"){
-      rsvp.style.display = 'flex'
-      var top = rsvp.offsetTop;
+      rsvp.style.display = 'flex';
+      let top = rsvp.offsetTop;
       window.scrollTo(10,top-50);
     }
     else if (this['id'] == "UsefulLinksButton"){
-      links.style.display = 'flex'
-      var top = links.offsetTop;
+      links.style.display = 'flex';
+      let top = links.offsetTop;
       window.scrollTo(10,top-50);
     }
 
@@ -110,8 +113,11 @@
 
   }
 
+  /**
+  jsdoc comment
+  */
   function initMap(){
-    let rockypoint = {lat: 31.3268, lng: -113.5312}
+    let rockypoint = {lat: 31.3268, lng: -113.5312};
     let map = new google.maps.Map(
       document.getElementById('map'), {zoom: 10, center: rockypoint}
     );
@@ -120,6 +126,9 @@
     );
   }
 
+  /**
+  jsdoc comment
+  */
   function submitClick(){
     let form = document.getElementById("form");
     let firstname = document.getElementById("firstname");
@@ -158,43 +167,49 @@
     });
     form.innerHTML = '';
     let a = document.createElement("h3");
-    a.innerHTML = "Thank you for your response!"
+    a.innerHTML = "Thank you for your response!";
     form.appendChild(a);
 
   }
-  var slideIndex = 0;
+  let slideIndex = 0;
+  /**
+  JSDOCCOMMENT
+  */
   function initGallery(){
-    var i;
-    var x = document.getElementsByClassName("mySlides");
+    let i;
+    let x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
+    if (slideIndex > x.length) {slideIndex = 1;}
     x[slideIndex-1].style.display = "block";
     setTimeout(initGallery, 2000); // Change image every 2 seconds
   }
+  /**
+  jsdoc comment
+  */
   function initCountDownTimer() {
-    var countDownDate = new Date("Oct 19, 2019 12:00:00").getTime();
+    let countDownDate = new Date("Oct 19, 2019 12:00:00").getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    let x = setInterval(function() {
 
       // Get todays date and time
-      var now = new Date().getTime();
+      let now = new Date().getTime();
 
       // Find the distance between now and the count down date
-      var distance = countDownDate - now;
+      let distance = countDownDate - now;
 
       // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       // Output the result in an element with id="demo"
       document.getElementById("countdowntimer").innerHTML = days + " : " + hours + " : "
-      + minutes + " : " + seconds ;
+      + minutes + " : " + seconds;
 
       // If the count down is over, write some text
       if (distance < 0) {
@@ -204,6 +219,11 @@
     }, 1000);
 
   }
+  /**
+  jsdoc comment
+  * @param {string} response the response.
+  * @returns {void}
+  */
   function checkStatus(response){
     let feedback = document.getElementById("feedback");
 
